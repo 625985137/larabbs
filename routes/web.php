a88878e6acbf98114cfc68bb8b07f742e6a263db
vagrant <vagrant@homestead>
2019-06-16 02:09:29 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('ver
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 //Route::get('/home', 'HomeController@index')->name('home');
+//个人中心
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
